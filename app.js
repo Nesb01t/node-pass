339dd3539613e74c0d0984e032b5d6175cfc16ee
nesb01t <nesb01t@163.com>
2024-10-10 11:12:07 +0800

    init

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+const baseUrl = process.env.BASE_URL;
+
 app.use("/", async (req, res) => {
   const path = req.path;
 
   const config = {
     method: "get",
-    url: `https://static.mercdn.net/${path}`,
-    responseType: "arraybuffer", // 确保接收到的是二进制数据
+    url: `${baseUrl}${path}`,
+    responseType: "arraybuffer",
     headers: {
       "User-Agent": "Apifox/1.0.0 (https://apifox.com)",
     },
@@ -18,8 +22,8 @@ app.use("/", async (req, res) => {
   try {
     const response = await axios.request(config);
     const contentType = response.headers["content-type"];
-    res.set("Content-Type", contentType); // 设置响应头
-    res.send(response.data); // 发送图片数据
+    res.set("Content-Type", contentType);
+    res.send(response.data);
   } catch (error) {
     res.status(500).send("Error fetching image");
   }
